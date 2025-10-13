4ac12e80ccb5d37ea33ea4f6cc1ea55832c88683
GabrielMay <44277883+gabrielmayek@users.noreply.github.com>
2025-10-13 09:18:20 -0500

    Cambios nuevos de mi pagina

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// let contacts = [{
+//     name: "Maxwell Wright",
+//     phone: "(0191) 719 6495",
+//     email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+// }, {
+//     name: "Raja Villarreal",
+//     phone: "0866 398 2895",
+//     email: "posuere.vulputate@sed.com"
+// }, {
+//     name: "Helen Richards",
+//     phone: "0800 1111",
+//     email: "libero@convallis.edu"
+// }];
+
+// console.log("el tamano de lista de contactos es de", contacts.length)
+// // console.log("Cual desea mostrar")
+// console.log("1-el primero");
+// console.log("2-el ultimo");
+// console.log("3-todos");
+// console.log("4-agregar");
+// let request = prompt("Cual desea mostrar");
+// switch (Number(request)) {
+//     case 1:
+//         console.log(contacts[0])
+//         break
+//     case 2:
+//         console.log(contacts[2])
+//         break;
+//     case 3:
+//         for (let contact of contacts) {
+//             console.log(contact)
+//         }
+//         break;
+//     case 4:
+//         let a = false;
+//         while (a != true) {
+
+//             let name = prompt("name")
+//             let phone = prompt("phone")
+//             let email = prompt("email")
+//             contacts.push(
+//                 {
+//                     name: name,
+//                     phone: phone,
+//                     email: email
+
+//                 }
+//             )
+//             start = confirm("desea guardar mas cosas")
+//             a = start === true ? false : true;
+
+//         }
+//         for (let contact of contacts) {
+//             console.log(contact)
+//         }
+//         break;
+//     default:
+//         console.log("elige una opcion valida");
+//         break;
+
+// }
+//
